e2400154d85af53a1c7165806976c273fcb5cedc
lyh221 <lyh221@naver.com>
2026-09-15 10:52:03 +0900

    변수선언

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 console.log("Hello js")
 
 // 변수: 값을 저장하는 이름을 가진 공간
-const username = "이유네";
+//const username = "이유네";
+let username = "이유네";
+
+
+username = "fnffnskssk";
 
 console.log("username",username)
+
+//데이터 타입
+const num = 10; //숫자
+console.log("🚀 ~ num:", num, "num type", typeof num)
+
+const text = "안녕하세요"
+console.log("🚀 ~ text:", text)
+
+const isDarkMode = true;
+console.log("🚀 ~ isDarkMode:", isDarkMode)
+console.log("🚀 ~ isDarkMode type:", typeof isDarkMode)
+
+const empty = null;
+console.log("🚀 ~ empty:", empty)
+console.log("🚀 ~ empty type:", typeof empty)
+
+let userage;
+console.log("🚀 ~ userage:", userage)
+
